57095b959666cca3190719c3aa71b5597df081bf
Mohamed Adel <ahmrn1999@gmail.com>
2023-06-25 13:00:29 +0200

    start

---
+++ src/Home/Home.jsx
@@ -0,0 +1,30 @@
+import { Container, Nav, Navbar } from "react-bootstrap"
+
+
+
+function Home(){
+
+
+    return(
+
+        <>
+        <Navbar bg="danger" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      
+
+
+
+
+        </>
+    )
+}
+
+export default Home
